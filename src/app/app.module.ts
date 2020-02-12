f2bead789d34f99dbdf6fad0c28e77277d0e4a9c
mikelbua <sapi_ma@hotmail.com>
2020-02-12 12:48:34 +0100

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,//modulo para llamadas por HTTP
+    FormsModule,//banana in a box
+    ReactiveFormsModule//para formularios reactivos
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/paginas/pokemon-res/pokemon-res.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-res',
+  templateUrl: './pokemon-res.component.html',
+  styleUrls: ['./pokemon-res.component.scss']
+})
+export class PokemonResComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
